d6535abf124b9d6d276299f2a994cc719f0bf308
Stefan Walkner <stefan@arkulpa.at>
2017-09-11 16:36:07 +0200

    Initial Commit

---
+++ Ttt/Ttt.h
@@ -0,0 +1,19 @@
+//
+//  Ttt.h
+//  Ttt
+//
+//  Created by Stefan Walkner on 11.09.17.
+//  Copyright © 2017 aobalarm GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ttt.
+FOUNDATION_EXPORT double TttVersionNumber;
+
+//! Project version string for Ttt.
+FOUNDATION_EXPORT const unsigned char TttVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ttt/PublicHeader.h>
+
+
